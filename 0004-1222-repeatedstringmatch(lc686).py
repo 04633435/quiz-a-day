3eb5e8b8d8b4eb5c29bbe0cb589d6c676528fbeb
yzd42 <yzd_340214@163.com>
2021-12-23 22:03:52 +0800

    add str hash

--- 0004-1222-repeatedstringmatch(lc686).py
+++ 0004-1222-repeatedstringmatch(lc686).py
@@ -1,6 +1,6 @@
 import math
 
-
+# solution-1
 class Solution:
     def repeated_string_match(self, a, b):
         n_a = len(a)
@@ -21,3 +21,12 @@ class Solution:
                 repeated_a = repeated_a + a
                 ans += 1
         return -1  
+
+
+# solution-2 strHash
+# instead of compare the char of a string, convert the string into a integer so that 
+# the comparision can be done in O(1).
+# Two things need to be kept in mind,
+# 1. choose a big number of m, so that the possibility of coillision will be enough small to ignored
+#    but not too big, because the overflow will slow down the comparision
+# 2. choose the prime number for both m and p, but the reason behind them not figured out yet.
