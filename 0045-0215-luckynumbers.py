ec444dbe4bee7e726dac76d9466f0d803dc01754
Zhedi Yang <yzd_340214@163.com>
2022-02-16 23:53:38 +0800

    modify 0215 argument name

--- 0045-0215-luckynumbers.py
+++ 0045-0215-luckynumbers.py
@@ -1,15 +1,15 @@
 class Solution:
-    def lucky_numbers(self, nums):
-        m = len(nums)
-        n = len(nums[0])
+    def lucky_numbers(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
         min_row = [float('inf')] * m
         max_col = [0] * n
         for i in range(m):
             for j in range(n):
-                if nums[i][j] < min_row[i]:
-                    min_row[i] = nums[i][j]
-                if nums[i][j] > max_col[j]:
-                    max_col[j] = nums[i][j]
+                if matrix[i][j] < min_row[i]:
+                    min_row[i] = matrix[i][j]
+                if matrix[i][j] > max_col[j]:
+                    max_col[j] = matrix[i][j]
 
         # return min_row
         ret = []
@@ -22,5 +22,5 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    nums = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
-    print(s.lucky_numbers(nums))
+    matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+    print(s.lucky_numbers(matrix))
